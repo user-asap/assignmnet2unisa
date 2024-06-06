f28b26e01bf0080a54068c7b6ab3459c5b704aa6
Arpit <aroitrabadiya48@gmail.com>
2024-06-06 14:33:14 +0530

    FR1: Calculate the patient alert levels

--- Patient.cpp
+++ Patient.cpp
@@ -1,11 +1,9 @@
 #include "Patient.h"
-
 #include "Vitals.h"
 
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-
 
 using namespace std;
 
@@ -14,8 +12,6 @@ const std::string Diagnosis::E_RUSH = "ERush";
 const std::string Diagnosis::NOCAP_SYNDROME = "NocapSyndrome";
 const std::string Diagnosis::TICCTOCC_BRAIN_DAMAGE = "TicctoccBrainDamage";
 
-
-
 Patient::Patient(const std::string& firstName, const std::string& lastName, std::tm birthday) :
     Person(firstName, lastName, birthday),
     _alertLevel(AlertLevel::Green)
@@ -23,7 +19,7 @@ Patient::Patient(const std::string& firstName, const std::string& lastName, std:
 }
 
 int Patient::age() const
-{	
+{
     // an inaccurate age estimate but fine for assignment purposes
     return 2024 - (1900 + _birthday.tm_year);
 }
@@ -32,22 +28,21 @@ std::string Patient::uid() const
 {
     std::stringstream ss;
     ss << static_cast<char>(std::tolower(_lastName.at(0)))
-       << static_cast<char>(std::tolower(_firstName.at(0)))
-       << std::setfill('0') << std::setw(2) << (_birthday.tm_mon + 1) 
-       << _birthday.tm_year;
+        << static_cast<char>(std::tolower(_firstName.at(0)))
+        << std::setfill('0') << std::setw(2) << (_birthday.tm_mon + 1)
+        << _birthday.tm_year;
     return ss.str();
 }
 
 std::string Patient::humanReadableID() const
 {
-
     return "" + _lastName + ", " + _firstName + " (" + uid() + ")";
 }
 
 std::ostream& operator<<(std::ostream& os, const Patient& p)
 {
     os << p.uid() << "/" << std::put_time(&p._birthday, "%d-%m-%Y") << "/" << p._lastName << "," << p._firstName << "/";
-    
+
     auto diagnoses = p.diagnoses();
     for (auto itr = diagnoses.begin(); itr != diagnoses.end(); ++itr) {
         if (itr != diagnoses.begin()) os << ";";
@@ -88,6 +83,7 @@ void Patient::addVitals(const Vitals* v)
 {
     _vitals.push_back(v);
     // TODO: calculate alert levels
+    calculateAlertLevel(v); // call function to calculate alert
 }
 
 const std::vector<const Vitals*> Patient::vitals() const
@@ -111,7 +107,42 @@ void Patient::setAlertLevel(AlertLevel level)
         case AlertLevel::Red:
             cout << "Red";
             break;
+        default:
+            break;
         }
         cout << endl;
     }
+    else {
+        cout << "Patient: " << humanReadableID() << " has an alert level: Green";
+    }
+}
+
+// function to calculate alert level
+void Patient::calculateAlertLevel(const Vitals* v)
+{
+    AlertLevel newLevel = AlertLevel::Green; // Default alert - Green
+
+    if (primaryDiagnosis() == Diagnosis::AMOGUS_SUS) { 
+        if (v->HR() > 220) newLevel = AlertLevel::Red;
+        else if (v->HR() > 210) newLevel = AlertLevel::Orange;
+        else if (v->HR() > 200) newLevel = AlertLevel::Yellow;
+    }
+    else if (primaryDiagnosis() == Diagnosis::E_RUSH) {
+        if (v->BT() > 38) {
+            if (v->BA() > 110) newLevel = AlertLevel::Red;
+            else if (v->BA() > 100) newLevel = AlertLevel::Yellow;
+        }
+    }
+    else if (primaryDiagnosis() == Diagnosis::NOCAP_SYNDROME) {
+        if (v->SPO2() < 90) newLevel = AlertLevel::Red;
+        else if (v->SPO2() < 92) newLevel = AlertLevel::Orange;
+        else if (v->SPO2() < 94) newLevel = AlertLevel::Yellow;
+    }
+    else if (primaryDiagnosis() == Diagnosis::TICCTOCC_BRAIN_DAMAGE) {
+        if ((age() < 35 && v->BA() < 10) || (age() >= 35 && v->BA() < 20)) {
+            newLevel = AlertLevel::Red;
+        }
+    }
+
+    setAlertLevel(newLevel);// setting alert level
 }
